603df21bf686e52044cf8c005c28f15d9b045ee0
Amardeep <amardeeps.shergill@gmail.com>
2025-04-22 17:40:33 +0100

    Create code for the project.sql

---
+++ code for the project.sql
@@ -0,0 +1,8 @@
+## *How many days have passed from the oldest Match to the most recent one*
+    Produce a table which, for each Season and League Name, shows the following statistics about the home goals scored: a) min b) average c) mid-range d) max e) sum Then confirm which combination of Season and League has the highest number of goals?
+    Find out how many unique seasons there are in the Match table. Then write a query that shows, for each Season, the number of matches played by each League. Do you notice anything out of the ordinary?
+    Using Players as the starting point, create a new table (PlayerBMI) and add: a) a new variable that represents the players’ weight in kg (divide the mass value by 2.205) and call it kg_weight; b) a variable that represents the height in metres (divide the cm value by 100) and call it m_height; c) a variable that shows the body mass index (BMI) of the player; d) Filter the table to show only the players with an optimal BMI (from 18.5 to 24.9). How many rows does this table have?
+    How many players do not have an optimal BMI?
+    Which Team has scored the highest total number of goals (home + away) during the most recent available season? How many goals has it scored?
+    Create a query that, for each season, shows the name of the team that ranks first in terms of total goals scored (the output table should have as many rows as the number of seasons). Which team was the one that ranked first in most of the seasons?
+    From question 8 create a new table (TopScorer) containing the top 10 teams in terms of total goals scored (hint: add the team id as well). Then write a query that shows all the possible “pair combinations” between those 10 teams. How many “pair combinations” did it generate?
